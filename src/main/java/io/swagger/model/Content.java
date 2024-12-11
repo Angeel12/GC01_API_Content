393a3673fd63fc5cb5e6c785ddd46654fc590947
Angeel12 <amaciasme@alumnos.unex.es>
2024-12-11 10:39:04 +0100

    SCRUM-239 ST79. Remove this unused import 'java.util.Arrays'.

--- src/main/java/io/swagger/model/Content.java
+++ src/main/java/io/swagger/model/Content.java
@@ -8,10 +8,8 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Content
